affb80fc099a5a0025c0474a1aab8b96c7d806d9
Ivan <ivanrumanchev95@gmail.com>
2021-09-03 01:49:11 +0300

    Выполнена Оптимизация графики всех страниц

--- source/js/app.js
+++ source/js/app.js
@@ -13,35 +13,105 @@ navToggle.addEventListener('click', function() {
   }
 });
 
-var toggleBefore = document.querySelector('.slider__toggle-before');
-var toggleAfter = document.querySelector('.slider__toggle-after');
-var sliderBefore = document.querySelector('.slider__item--before');
-var sliderAfter = document.querySelector('.slider__item--after');
+// var toggleBefore = document.querySelector('.slider__toggle--before');
+// var toggleAfter = document.querySelector('.slider__toggle--after');
+// var sliderBefore = document.querySelector('.slider__item--before');
+// var sliderAfter = document.querySelector('.slider__item--after');
 
-toggleAfter.addEventListener('click', function() {
-  if (sliderBefore.classList.contains('slider__item--current')) {
-    sliderBefore.classList.add('visually-hidden');
-    sliderAfter.classList.remove('visually-hidden');
-    sliderBefore.classList.remove('slider__item--current');
-    sliderAfter.classList.add('slider__item--current');
+// toggleAfter.addEventListener('click', function() {
+//   if (sliderBefore.classList.contains('slider__item--current')) {
+//     sliderBefore.classList.add('visually-hidden');
+//     sliderAfter.classList.remove('visually-hidden');
+//     sliderBefore.classList.remove('slider__item--current');
+//     sliderAfter.classList.add('slider__item--current');
 
-    toggleBefore.classList.remove('slider__toggle-before--checked');
-    toggleAfter.classList.add('slider__toggle-after--checked');
-    toggleBefore.classList.add('slider__toggle--check');
-    toggleAfter.classList.remove('slider__toggle--check');
+//     toggleBefore.classList.remove('slider__toggle--before--checked');
+//     toggleAfter.classList.add('slider__toggle--after--checked');
+//     toggleBefore.classList.add('slider__toggle--check');
+//     toggleAfter.classList.remove('slider__toggle--check');
+//   }
+// });
+
+// toggleBefore.addEventListener('click', function() {
+//   if (sliderAfter.classList.contains('slider__item--current')) {
+//     sliderAfter.classList.add('visually-hidden');
+//     sliderBefore.classList.remove('visually-hidden');
+//     sliderAfter.classList.remove('slider__item--current');
+//     sliderBefore.classList.add('slider__item--current');
+
+//     toggleAfter.classList.remove('slider__toggle--after--checked');
+//     toggleBefore.classList.add('slider__toggle--before--checked');
+//     toggleAfter.classList.add('slider__toggle--check');
+//     toggleBefore.classList.remove('slider__toggle--check');
+//   }
+// });
+
+const mediaQuery = window.matchMedia('(min-width: 768px)');
+let scopeMobile = 14;
+let scopeTablet = 15.3;
+let widthIconMobile = 57;
+let heightIconMobile = 53;
+let widthIconTablet = 113;
+let heightIconTablet = 106;
+let iconImageOffsetXMobile = -25;
+let iconImageOffsetYMobile = -45;
+let iconImageOffsetXTablet = -50;
+let iconImageOffsetYTablet = -60;
+
+var scope = scopeMobile;
+var widthIcon = widthIconMobile;
+var heightIcon = heightIconMobile;
+var iconImageOffsetX = iconImageOffsetXMobile;
+var iconImageOffsetY = iconImageOffsetYMobile;
+
+function handleTabletChange(e) {
+  if (e.matches) {
+    scope = scopeTablet;
+    widthIcon = widthIconTablet;
+    heightIcon = heightIconTablet;
+    iconImageOffsetX = iconImageOffsetXTablet;
+    iconImageOffsetY = iconImageOffsetYTablet;
+
+  } else {
+    scope = scopeMobile;
+    widthIcon = widthIconMobile;
+    heightIcon = heightIconMobile;
+    iconImageOffsetX = iconImageOffsetXMobile;
+    iconImageOffsetY = iconImageOffsetYMobile;
   }
-});
+}
 
-toggleBefore.addEventListener('click', function() {
-  if (sliderAfter.classList.contains('slider__item--current')) {
-    sliderAfter.classList.add('visually-hidden');
-    sliderBefore.classList.remove('visually-hidden');
-    sliderAfter.classList.remove('slider__item--current');
-    sliderBefore.classList.add('slider__item--current');
+handleTabletChange(mediaQuery)
 
-    toggleAfter.classList.remove('slider__toggle-after--checked');
-    toggleBefore.classList.add('slider__toggle-before--checked');
-    toggleAfter.classList.add('slider__toggle--check');
-    toggleBefore.classList.remove('slider__toggle--check');
-  }
+ymaps.ready(function () {
+  var myMap = new ymaps.Map('map', {
+          center: [59.938635, 30.323118],
+          zoom: scope
+      }, {
+          searchControlProvider: 'yandex#search'
+      }),
+
+      // Создаём макет содержимого.
+      MyIconContentLayout = ymaps.templateLayoutFactory.createClass(
+          '<div style="color: #FFFFFF; font-weight: bold;">$[properties.iconContent]</div>'
+      ),
+
+      myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
+          hintContent: 'Cat Energy',
+          balloonContent: 'Приходите в гости :)'
+      }, {
+          // Опции.
+          // Необходимо указать данный тип макета.
+          iconLayout: 'default#image',
+          // Своё изображение иконки метки.
+          iconImageHref: 'img/mappin.svg',
+          // Размеры метки.
+          iconImageSize: [widthIcon, heightIcon],
+          // Смещение левого верхнего угла иконки относительно
+          // её "ножки" (точки привязки).
+          iconImageOffset: [iconImageOffsetX, iconImageOffsetY]
+      });
+
+  myMap.geoObjects
+      .add(myPlacemark);
 });
